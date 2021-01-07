781ab9310a97ad7747b27be7f1a377c8ff146d2e
AldaVM <aldairvm95@gmail.com>
2021-01-06 23:37:50 -0500

    servicio entorno de produción

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-// export const URL_API = "https://imperium-backend.herokuapp.com/v1/api";
-export const URL_API = "http://localhost:8000/v1/api";
+export const URL_API = "https://imperium-backend.herokuapp.com/v1/api";
+// export const URL_API = "http://localhost:8000/v1/api";
